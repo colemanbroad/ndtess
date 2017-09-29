9c228450eb1011b2504ab506712912198beca5c3
Peter Steinbach <steinbac@mpi-cbg.de>
2017-09-29 10:00:21 +0200

    refactored scalar function into its own header

---
+++ include/scalar_math.cpp
@@ -0,0 +1,18 @@
+#ifndef SCALAR_MATH_H
+#define SCALAR_MATH_H
+
+#include <iostream>
+
+namespace ndtess {
+
+    namespace scalar {
+        int add(int i, int j) {
+            std::cout << "hey this is C++!\n";
+            return i + j;
+        }
+
+    }
+
+};
+
+#endif /* SCALAR_MATH_H */
