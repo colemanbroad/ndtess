3e3863e3c3dba7422258325c67bdd0b7a333c6f3
Peter Steinbach <steinbac@mpi-cbg.de>
2017-09-29 10:05:27 +0200

    setuptools driven build works, tests are ignored

--- tests/cpp/test_cpp_add_scalar.cpp
+++ tests/cpp/test_cpp_add_scalar.cpp
@@ -1,11 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "ndtess_scalar.hpp"
+#include "scalar_math.hpp"
 
 TEST_CASE( "tests on scalars", "[scalars]" ) {
 
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+    REQUIRE( ndtess::scalar::add(1,1) == 2 );
 }
