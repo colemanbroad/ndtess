93dd6c1e439670cc21604154753b1c0aa573288d
Peter Steinbach <steinbac@mpi-cbg.de>
2017-09-29 10:00:45 +0200

    include functionality by header

--- src/ndtess.cpp
+++ src/ndtess.cpp
@@ -1,13 +1,7 @@
-#include <iostream>
 #include <pybind11/pybind11.h>
-
-int add_scalars(int i, int j) {
-	std::cout << "hey this is C++!\n";
-    return i + j;
-}
+#include "scalar_math.hpp"
 
 PYBIND11_MODULE(ndtess, m) {
-    m.doc() = "simple function to add 2 scalars";
-
-    m.def("add_scalars", &add_scalars, "A function which adds two numbers");
+    m.doc() = "simple function to add two scalars";
+    m.def("add_scalars", &ndtess::scalar::add, "A function which adds two numbers");
 }
