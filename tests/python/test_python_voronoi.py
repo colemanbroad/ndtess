0fbfffe3f5e1bc05c0d6b1280745b931ba806c21
Coleman Broaddus <broaddus@mpi-cbg.de>
2017-10-26 11:03:52 +0200

    Test on randomly generated example images.

--- tests/python/test_python_voronoi.py
+++ tests/python/test_python_voronoi.py
@@ -36,12 +36,19 @@ def load_synthetic():
         img[x,ylo:yhi] = 100
         classimg[x,ylo:yhi,2] = 3 #circles are class 3
 
+    ## label image is build with labeled seeds
+    ## we could also do this by using `scipy.ndimage.label()` on a binary mask...
     labimg = np.zeros(img.shape,dtype=np.int8)
-    for i in range(3):
-        labimg = labimg + classimg[:,:,i]
+    seeds0 = np.random.randint(0, labimg.shape[0], 11)
+    seeds1 = np.random.randint(0, labimg.shape[1], 11)
+    labimg[seeds0, seeds1] = np.arange(1, 11+1, dtype=np.int8)
 
-    #not sure yet, how to generate an image with see points?
-    distimg = np.zeros(img.shape)
+    ## the "distance" between adjacent pixels (i0,i1) and (j0,j1) in labimg
+    ## is given by `d = distimg[i0,i1] + distimg[j0,j1]`, thus distimg must be
+    ## everywhere > 0
+    distimg = np.random.rand(*labimg.shape)*3.3
+
+    assert np.alltrue(distimg >= 0)
 
     return { 'img' : img, 'distimg' : distimg, 'lab' : labimg, 'result' : classimg  }
 
@@ -50,8 +57,16 @@ def test_tessellate_on_synth_image(load_synthetic):
     """
     run the tesselation on a labelled image where all objects have been identified already (null test)
     """
-    vorimg = tess.tessellate_labimg(load_synthetic['lab']) #, load_state['distimg'])
-    assert vorimg == load_synthetic['result']
+    vorimg = tess.tessellate_labimg(load_synthetic['lab'])    #, load_state['distimg'])
+    vorimg2 = tess.tessellate_labimg(load_synthetic['lab'], load_synthetic['distimg'])
+    mask = load_synthetic['lab']!=0
+    
+    ## Obviously, this is not a complete test for correctness, but it will catch the simplest bugs.
+    assert np.alltrue(vorimg[mask] == load_synthetic['lab'][mask])
+    assert np.alltrue(vorimg2[mask] == load_synthetic['lab'][mask])
+    ## The results are correct according to visual inspection :)
+    # return vorimg, vorimg2
+    # assert vorimg == load_synthetic['result']
 
 def test_tessellate_on_null_image(load_synthetic):
     """
