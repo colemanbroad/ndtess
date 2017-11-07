f9196f6e99959a07844fbe3d80edc96d68e3f194
Peter Steinbach <steinbac@mpi-cbg.de>
2017-11-07 15:55:50 +0100

    refactored ndtess interface

--- include/tesselate.hpp
+++ include/tesselate.hpp
@@ -1,6 +1,8 @@
 #ifndef TESSELATE_H
 #define TESSELATE_H
 
+#include <queue>
+
 #include "pybind11/pybind11.h"
 #include "pybind11/numpy.h"
 
@@ -10,12 +12,33 @@ namespace ndtess {
 
     namespace tesselate {
 
-        py::array_t<double> labimg(py::array_t<double> input) {
+        py::array_t<double> labimg_impl(const py::array_t<double>& input,
+                                        const py::array_t<double>& distimg) {
 
-            auto buf = input.unchecked();
+            auto mbuf = input.unchecked();
 
             //TODO: how to create an array with the SHAPE than input?
             auto result = py::array_t<double>(input);
+
+            std::vector<std::size_t> shape(input.ndim(), 0);
+            int c = 0;
+            for(std::size_t & el: shape)
+                el = input.shape(c++);
+
+            //initialize heap queue
+
+            return result;
+
+        }
+
+        py::array_t<double> labimg(const py::array_t<double>& input) {
+
+            auto distimg = py::array_t<double>(input);
+            auto mbuf = distimg.mutable_unchecked();
+
+            std::fill(&mbuf[0], &mbuf[0] + distimg.size(),0.);
+
+            auto result = labimg_impl(input, distimg);
 
             return result;
         }
