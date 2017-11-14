2edc492f2825ec259f556ac8a90dbb375fb1dfd3
Peter Steinbach <steinbac@mpi-cbg.de>
2017-11-14 11:03:12 +0100

    added tesselate implementation

--- include/tesselate.hpp
+++ include/tesselate.hpp
@@ -1,10 +1,12 @@
 #ifndef TESSELATE_H
 #define TESSELATE_H
 
-#include <queue>
+#include "heapqueue.hpp"
 
 #include "pybind11/pybind11.h"
 #include "pybind11/numpy.h"
+
+#include <queue>
 
 namespace py = pybind11;
 
@@ -12,8 +14,9 @@ namespace ndtess {
 
     namespace tesselate {
 
+        template <typename T>
         py::array_t<double> labimg_impl(const py::array_t<double>& input,
-                                        const py::array_t<double>& distimg) {
+                                        const py::array_t<T>& distimg) {
 
             auto mbuf = input.unchecked();
 
@@ -26,6 +29,11 @@ namespace ndtess {
                 el = input.shape(c++);
 
             //initialize heap queue
+            const double* in = reinterpret_cast<const double*>(input.unchecked().data(0));
+            const T* di = reinterpret_cast<const T*>(distimg.unchecked().data(0));
+            auto heap = heap::build(in,
+                                    shape,
+                                    di);
 
             return result;
 
@@ -33,7 +41,7 @@ namespace ndtess {
 
         py::array_t<double> labimg(const py::array_t<double>& input) {
 
-            auto distimg = py::array_t<double>(input);
+            auto distimg = py::array_t<float>(input);
             auto mbuf = distimg.mutable_unchecked();
 
             std::fill(&mbuf[0], &mbuf[0] + distimg.size(),0.);
