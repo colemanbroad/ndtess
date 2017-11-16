187574d671a59b44fc5330d402e190c33ca34662
Peter Steinbach <steinbac@mpi-cbg.de>
2017-11-16 13:47:09 +0100

    fixed variables

--- tests/python/tesselations.py
+++ tests/python/tesselations.py
@@ -18,20 +18,42 @@ NEIGHBOR_GRID = np.array([(-1, 0), (1, 0), (0, -1), (0, 1)])
 def _inbounds(img):
     ymax, xmax = img.shape
     def f(y,x):
-        return 0<=x<xmax and 0<=y<ymax
+        return 0<=y<ymax and 0<=x<xmax
     return f
 
 def distance(f0,f1):
     return np.abs(f0+f1) + 1.0
 
 # ---- 
+def _nonrow_major_inbounds(img):
+    ymax, xmax = img.shape
+    def f(x,y):
+        return 0<=x<xmax and 0<=y<ymax
+    return f
 
-def initialize_heapq(labimg, distimg):
+def initialize_heapq(labimg, distimg, row_major = True):
     heap=[]
     heapq.heapify(heap)
     inbounds = _inbounds(labimg)
+    if not row_major:
+        inbounds = _nonrow_major_inbounds(labimg)
+    print()
+    if row_major:
+        for y in range(labimg.shape[0]):
+            for x in range(labimg.shape[1]):
+                l = labimg[y,x]
+                if l != 0:
+                    for dy,dx in NEIGHBOR_GRID:
+                        y2,x2 = y+dy, x+dx
+                        if inbounds(y2,x2):
+                            d1 = distimg[y,x]
+                            d2 = distimg[y2,x2]
+                            dist = distance(d1,d2)
 
-    for x in range(labimg.shape[0]):
+                            heapq.heappush(heap, (dist, x2, y2 , l))
+                            print(">> (%3i %3i) %4.1f (%3i %3i) %4.1f %4.1f -> %s" % (y,x,d1,y2,x2,d2,l,str(heap[0])))
+    else:
+        for x in range(labimg.shape[0]):
          for y in range(labimg.shape[1]):
              l = labimg[x,y]
              if l != 0:
@@ -43,82 +65,59 @@ def initialize_heapq(labimg, distimg):
                          dist = distance(d1,d2)
                          #print(">> (%3i %3i) %4.1f (%3i %3i) %4.1f %4.1f" % (y,x,d1,y2,x2,d2,l))
                          heapq.heappush(heap, (dist, x2, y2, l))
-
-    # for y in range(labimg.shape[0]):
-    #     for x in range(labimg.shape[1]):
-    #         l = labimg[y,x]
-    #         if l != 0:
-    #             for dx,dy in NEIGHBOR_GRID:
-    #                 x2,y2 = x+dx, y+dy
-    #                 if inbounds(y2,x2):
-    #                     d1 = distimg[y,x]
-    #                     d2 = distimg[y2,x2]
-    #                     dist = distance(d1,d2)
-    #                     heapq.heappush(heap, (dist, x2, y2, l))
     return heap
 
-def initialize_heapq_coords(coords, labels, distimg):
-    """
-    An alternative heapq initialization, if you want to initialize from a number
-    of labeled seed points, instead of growing pre-existing label regions
-    """
-    boundary_value = labels.max() + 1
 
-    vorimg = np.zeros(distimg.shape)
-    coords = coords.astype('int')
-    vorimg[coords[:,0], coords[:,1]] = labels
-    vorimg = vorimg.astype('uint16')
-    vorimg[0,:]  = boundary_value
-    vorimg[-1,:] = boundary_value
-    vorimg[:,0]  = boundary_value
-    vorimg[:,-1] = boundary_value
-
-    heap=[]
-    heapq.heapify(heap)
-
-    for i in range(len(coords)):
-        x,y = coords[i]
-        l   = labels[i]
-        for dx,dy in NEIGHBOR_GRID:
-            x2,y2 = x+dx, y+dy
-            d1 = distimg[x,y]
-            d2 = distimg[x2, y2]
-            dist = distance(d1,d2)
-            heapq.heappush(heap, (dist, x2, y2, l))
-    return heap, vorimg
-
-def build_vorimg(heap, vorimg_0, distimg):
+def build_vorimg(heap, vorimg_0, distimg, row_major = True):
     """
     tessellate an image by growing existing labeled regions
     """
     vorimg = vorimg_0.copy()
 
     inbounds = _inbounds(vorimg)
+    if not row_major:
+        inbounds = _nonrow_major_inbounds(vorimg)
     #print()
-    while len(heap) > 0:
-        d,x,y,l = heapq.heappop(heap)
-        #print("-- %4.1f %4i %4i %4.1f" % (d,x,y,l))
-        if vorimg[x,y]==0:
-            vorimg[x,y] = l
-            for dx, dy in NEIGHBOR_GRID:
-                x2,y2 = x+dx, y+dy
-                if inbounds(x2,y2):
-                    nl = vorimg[x2, y2]
-                    if nl == 0 : # then unclaimed
-                        d1 = distimg[x,y]
-                        d2 = distimg[x2, y2]
-                        nd = distance(d1,d2) + d
-                        heapq.heappush(heap, (nd, x2, y2, l))
-                        #print("++ %4.1f %4i %4i %4.1f" %(nd,x2,y2,l))
+
+    if row_major:
+        while len(heap) > 0:
+            d,x,y,l = heapq.heappop(heap)
+            if vorimg[y,x]==0:
+                vorimg[y,x] = l
+                for dy,dx in NEIGHBOR_GRID:
+                    x2,y2 = x+dx, y+dy
+                    if inbounds(y2,x2):
+                        nl = vorimg[y2,x2]
+                        if nl == 0 : # then unclaimed
+                            d1 = distimg[y,x]
+                            d2 = distimg[y2,x2]
+                            nd = distance(d1,d2) + d
+                            heapq.heappush(heap, (nd, x2, y2, l))
+
+    else:
+        while len(heap) > 0:
+            d,x,y,l = heapq.heappop(heap)
+            if vorimg[x,y]==0:
+                vorimg[x,y] = l
+                for dx, dy in NEIGHBOR_GRID:
+                    x2,y2 = x+dx, y+dy
+                    if inbounds(x2,y2):
+                        nl = vorimg[x2, y2]
+                        if nl == 0 : # then unclaimed
+                            d1 = distimg[x,y]
+                            d2 = distimg[x2, y2]
+                            nd = distance(d1,d2) + d
+                            heapq.heappush(heap, (nd, x2, y2, l))
+
     return vorimg
 
 # ----
 
-def tessellate_labimg(labimg, distimg=None):
-    print("[tessellate_labimg] :: ",labimg.shape)
+def tessellate_labimg(labimg, distimg=None, row_major= True):
+    #print("[tessellate_labimg] :: ",labimg.shape)
     if distimg is None:
         distimg = np.zeros_like(labimg).astype('float32')
-    heap = initialize_heapq(labimg, distimg)
-    vorimg = build_vorimg(heap, labimg, distimg)
+    heap = initialize_heapq(labimg, distimg, row_major)
+    vorimg = build_vorimg(heap, labimg, distimg, row_major)
     return vorimg
 
