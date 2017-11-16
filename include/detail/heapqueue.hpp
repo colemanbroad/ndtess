1d7f667608527d145d6fe581c89b8b8be6abaa01
Peter Steinbach <steinbac@mpi-cbg.de>
2017-11-16 13:46:54 +0100

    added traces

--- include/detail/heapqueue.hpp
+++ include/detail/heapqueue.hpp
@@ -31,50 +31,51 @@ namespace ndtess {
                                                     std::multiplies<std::size_t>()
                 );
 
-            const T nil = 0;
-
-            //FOR LATER: check if this cache miss due to first jumping in y and then jumping in x
-            //           needs some attention
-            static constexpr int offsets_y[4] = {-1,1,0,0};
-            static constexpr int offsets_x[4] = {0,0,-1,1};
-
             std::int64_t x2 = 0;
             std::int64_t y2 = 0;
             static constexpr T epsilon = std::numeric_limits<T>::epsilon();
 
             pvec<T> under_heap;
             queue q;
-            std::cout.precision(1);
-            for(std::int64_t y = 0;y < _shape[1];++y){
-                for(std::int64_t x = 0;x < _shape[0];++x){
 
-                    std::size_t pix_offset = y*_shape[1] + x;
+#ifdef NDTESS_TRACE
+            std::cout.precision(1);
+#endif
+            for(std::int64_t y = 0;y < _shape[ndtess::in_y];++y){
+                for(std::int64_t x = 0;x < _shape[ndtess::in_x];++x){
+
+                    std::size_t pix_offset = y*_shape[ndtess::in_x] + x;
                     if(std::abs(_lab[pix_offset]) < epsilon)
                         continue;
 
                     for(int off = 0;off < 4;++off){
 
-                        x2 = x + offsets_x[off];
-                        y2 = y + offsets_y[off];
-                        if(!( x2 >= 0 && x2 < _shape[1] && y2 >= 0 && y2 < _shape[0] ))
+                        x2 = x + ndtess::offsets_x[off];
+                        y2 = y + ndtess::offsets_y[off];
+                        if(!( x2 >= 0 && x2 < _shape[ndtess::in_x] && y2 >= 0 && y2 < _shape[ndtess::in_y] ))
                             continue;
 
-                        float res = 1. + std::abs(_dist[pix_offset] + _dist[(y2)*_shape[1] + (x2)]);
+                        auto d1 = _dist[pix_offset];
+                        auto d2 = _dist[(y2)*_shape[ndtess::in_x] + (x2)];
+                        auto res = std::abs(d1 + d2) + 1.f;
+
+#ifdef NDTESS_TRACE
                         std::cout << "<> "
                                   << "(" << std::setw(3) << y
                                   << std::setw(3) << x
                                   << ")"
-                                  << std::setw(4) << _dist[pix_offset] << " "
+                                  << std::setw(4) << d1 << " "
                                   << "(" << std::setw(3) << y2
                                   << std::setw(3) << x2
                                   << ")"
-                                  << std::setw(4) << _dist[(y2)*_shape[1] + (x2)]
+                                  << std::setw(4) << d2
                                   << " " << _lab[pix_offset];
-
+#endif
                         q.push(std::make_tuple(res,
                                                x2,
                                                y2,
                                                _lab[pix_offset]));
+#ifdef NDTESS_TRACE
                         auto t = q.top();
                         std::cout << " -> "
                                   << std::get<0>(t) << ", "
@@ -82,6 +83,7 @@ namespace ndtess {
                                   << std::get<2>(t) << ", "
                                   << std::get<3>(t) << " " << q.size()
                                   << "\n";
+#endif
                     }
                 }
             }
